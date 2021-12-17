ec59f3edd79588d612045365d2285ead9955ac90
Javi1427 <javierrecinos27@gmail.com>
2021-12-17 09:48:59 -0600

    Creacion de DB y scrpts de control

---
+++ Scripts/Control_proyects.sql
@@ -0,0 +1,30 @@
+LOAD DATA
+LOCAL INFILE '/home/javier/practica1/Practica1_MIA/ArchivosCSV/projects.csv'
+INTO TABLE vacas.temp_projects
+CHARACTER SET latin1
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(	project_id,
+	is_geocoded,
+	project_title,
+	start_actual_isodate,
+	end_actual_isodate,
+	donors,
+	donors_iso3,
+	recipients,
+	recipients_iso3,
+	ad_sector_codes,
+	ad_sector_names,
+	status,
+	transactions_start_year,
+	transactions_end_year,
+	total_commitments,
+	total_disbursements
+);
+
+SELECT COUNT(*) FROM vacas.PROJECTS;
+DELETE FROM vacas.temp_projects;
+SELECT * FROM vacas.temp_projects;
+SELECT * FROM vacas.PROJECT;
